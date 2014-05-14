82db6e2b9071895ec70ba47185afc5764e0f4aa5
Eli Reisman <ereisman@apache.org>
2014-05-13 18:25:58 -0700

    Add GenericOptionsParser/DistributedCache wiring to our Hadoop Tool impl

--- src/main/scala/com/ereisman/esurient/hadoop/EsurientTool.scala
+++ src/main/scala/com/ereisman/esurient/hadoop/EsurientTool.scala
@@ -1,14 +1,18 @@
 package com.ereisman.esurient.hadoop
 
 
-import org.apache.hadoop.util.{Tool,ToolRunner}
+import org.apache.hadoop.util.{Tool,ToolRunner,GenericOptionsParser}
 import org.apache.hadoop.conf.{Configuration,Configured}
 
 
 object EsurientTool {
 
   def main(args: Array[String]): Unit = {
-    System.exit(ToolRunner.run(new Configuration, new EsurientTool, args))
+    val conf = new Configuration(true)
+    val nonHadoopArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+    System.exit(
+      ToolRunner.run(conf, new EsurientTool, nonHadoopArgs)
+    )
   }
 }
 
@@ -16,8 +20,9 @@ object EsurientTool {
 class EsurientTool extends Configured with Tool {
 
   override def run(args: Array[String]): Int = {
-    // TODO: parse the cmd line args (GenericOptionsParser + custom args)
-    //       then add these to the Configuration and run the Job
-    0 // return for now
+    // TODO: parse args that were not delivered to "hadoop jar" commmand
+    //       by the GenericOptionsParser, add those values to the Configuration
+    //       and kick off the job run!
+    0
   }
 }
