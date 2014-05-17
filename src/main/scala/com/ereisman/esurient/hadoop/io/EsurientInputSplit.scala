93664c7b3bcb518a32d708012cc3aced67ddff40
Eli Reisman <ereisman@apache.org>
2014-05-17 12:00:57 -0700

    add no-arg ctor to input split for reflection

--- src/main/scala/com/ereisman/esurient/hadoop/io/EsurientInputSplit.scala
+++ src/main/scala/com/ereisman/esurient/hadoop/io/EsurientInputSplit.scala
@@ -9,9 +9,12 @@ import java.io.{DataInput,DataOutput}
  * Dummy class to fake out Hadoop so we can ask for a
  * fixed number of Mapper tasks to run our ETL code in.
  */
-class EsurientInputSplit(var splitId: Int = -1, var numSplits: Int = -1) extends InputSplit with Writable {
-
+class EsurientInputSplit(var splitId: Int, var numSplits: Int) extends InputSplit with Writable {
+  
+  def this() = { this(-1, -1) }
+  
   override def getLength = 0L
+
   override def getLocations = Array[String]()
 
   override def readFields(in: DataInput): Unit = {
@@ -23,9 +26,6 @@ class EsurientInputSplit(var splitId: Int = -1, var numSplits: Int = -1) extends
     out.writeInt(splitId)
     out.writeInt(numSplits)
   }
-
-  def getSplitId = splitId
-  def getNumSplits = numSplits
 
   override def toString = getClass.getName + ": task " + splitId + " of " + numSplits
 }
