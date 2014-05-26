ee776ef6d3ef19aa62666d9bfbaf52868fb633b7
Eli Reisman <ereisman@apache.org>
2014-05-26 11:26:46 -0700

    align the example job more closely with whats in the example props file, pretty up the log output

--- src/main/scala/com/ereisman/esurient/examples/EsurientPartitionedTask.scala
+++ src/main/scala/com/ereisman/esurient/examples/EsurientPartitionedTask.scala
@@ -18,37 +18,37 @@ class EsurientPartitionedTask extends com.ereisman.esurient.EsurientTask {
   import com.ereisman.esurient.examples.EsurientPartitionedTask._
 
   override def execute: Unit = {
-    // the Hadoop Configuration for this job full of useful metadata, and user-set job properties
+    // the Hadoop Configuration for this job. Populated with useful metadata,
+    // user-defined job properties, and a unique TaskId. You can use the
+    // Configuration to obtain a FileSystem object (to access HDFS)
+    // and much more (see Hadoop documentation)
     val conf = context.getConfiguration
 
-    // the unique task id you can use to assign work deterministically to each task
+    // use the Hadoop Configuration to obtain the unique task ID the framework
+    // has assigned this process. You can use it to assign work deterministically.
     val taskId = conf.getInt(ES_THIS_TASK_ID, ERROR)
-    // use modular arithmetic and the monotonic integers assigned as 
+    // can use modular arithmetic and the monotonic integers assigned as 
     // taskId's to partition the tasks in this job into various work groups
     val groupId = taskId % conf.getInt(ES_TASK_GROUPS, 4)
     // total # of tasks launched in this job, in this case set by the esurient-example-job.properties file
     val totalTasks = conf.getInt(ES_TASK_COUNT, ERROR)
+
     // retrieve another user-defined value set in properties file, injected into Configuration,
     // that has no meaning outside the user's application class:
-    val myUserDefinedValue = conf.getInt("user.job.arg.1", 0)
+    val myUserDefinedValueOne = conf.getInt("esurient.user.job.arg.1", 0)
+    val myUserDefinedValueTwo = conf.get("esurient.user.job.arg.2", "ERROR")
     // same, but this time its a String value
     val myOtherUserDefinedValue = conf.get("can.be.called.anything", "error, no value set?")
 
     // do some hello world stuff to demonstrate using Configuration values
     // to assign task and group work deterministically (if desired)
-    LOG.info("Hi, This is EsurientDefaultTask #" + taskId + " of " + totalTasks)
-    
+    LOG.info("Hi, This is EsurientDefaultTask #" + taskId + " of " + totalTasks)    
     // partition tasks up into work groups by their task ID.
-    LOG.info("Task #" + taskId + "is a member of work group #" + groupId)
-    LOG.info( groupId match {
-      case 0 => "My job is to process files A-F"
-      case 1 => "My job is to process files G-K"
-      case 2 => "My job is to process files L-P"
-      case 3 => "My job is to process files Q-Z"
-    })
+    LOG.info("Task (" + taskId + ") is a member of work group (" + groupId + ")")
 
-    // these are the random user defined values from the job properties file
-    LOG.info("user.job.arg.1 from example properties file has value: " + myUserDefinedValue)
+    // these are some user defined values from the example job properties file
+    LOG.info("esurient.user.job.arg.1 from example properties file has value: " + myUserDefinedValueOne)
+    LOG.info("esurient.user.job.arg.2 from example properties file has value: " + myUserDefinedValueTwo)
     LOG.info("can.be.called.anything from example properties file has value: " + myOtherUserDefinedValue)
     
     // lets make task #7 (if there is one) do something specific, act as a "master" node, whatever
